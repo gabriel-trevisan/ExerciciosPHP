bdb8924cbd4dbf83565c7f3e32f31e44c90bac06
Gabriel Trevisan <gabriel.trevisan.santos@gmail.com>
2020-05-12 13:56:57 -0300

    Add more functions and tests

--- src/Easy.php
+++ src/Easy.php
@@ -2,14 +2,52 @@
 
     function sum($num1, $num2){
         /* A resolução deve ser realizada aqui */
+        return;
     }
 
     function area($num1, $num2){
         /* A resolução deve ser realizada aqui */
+        return;
     }
 
     function getLastItem(array $arr){
         /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function check(array $arr, $num){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function animals($numGalinhas, $numVacas, $numPorcos){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function smallNumber($num1, $num2){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function findSmallestNumber(array $arr){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function missNumber(array $arr){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function isChristmas($date){
+        /* A resolução deve ser realizada aqui */
+        return;
+    }
+
+    function isValidEmail($email){
+        /* A resolução deve ser realizada aqui */
+        return;
     }
 
 ?>
--- tests/EasyTest.php
+++ tests/EasyTest.php
@@ -27,6 +27,50 @@
             $this->assertEquals(15, getLastItem([5, 6, 15]));
         }
 
+        public function testCheck(){
+            $this->assertEquals(true, check([1, 2, 3, 4, 5], 5));
+            $this->assertEquals(false, check([1, 1, 2, 1, 1], 3));
+            $this->assertEquals(false, check([], 2));
+        }
+
+        public function testAnimals(){
+            $this->assertEquals(36, animals(2, 3, 5));
+            $this->assertEquals(22, animals(1, 2, 3));
+            $this->assertEquals(50, animals(5, 2, 8));
+        }
+
+        public function testSmallNumber(){
+            $this->assertEquals(20, smallNumber(20, 30));
+            $this->assertEquals(5, smallNumber(10, 5));
+            $this->assertEquals(8, smallNumber(20, 8));
+        }
+
+        public function testFindSmallestNumber(){
+            $this->assertEquals(3, findSmallestNumber([10, 3, 9, 4, 7]));
+            $this->assertEquals(2, findSmallestNumber([5, 9, 3, 10, 2]));
+            $this->assertEquals(5, findSmallestNumber([20, 7, 8, 6, 5]));
+        }
+
+        public function testMissNumber(){
+            $this->assertEquals(5, missNumber([10, 9, 8, 7, 6, 4, 3, 2, 1]));
+            $this->assertEquals(3, missNumber([9, 10, 8, 7, 6, 5, 1, 2, 4]));
+            $this->assertEquals(1, missNumber([7, 6, 8, 10, 9, 5, 4, 3, 2]));
+        }
+
+        public function testIsChristmas(){
+            $this->assertEquals(true, isChristmas(new DateTime("2010-12-25")));
+            $this->assertEquals(true, isChristmas(new DateTime("2050-12-25")));
+            $this->assertEquals(false, isChristmas(new DateTime("2020-05-11")));
+        }
+
+        public function testIsValidEmail(){
+            $this->assertEquals(false, isValidEmail("@gmail.com"));
+            $this->assertEquals(false, isValidEmail("gabriel@com"));
+            $this->assertEquals(false, isValidEmail("gmail"));
+            $this->assertEquals(false, isValidEmail("gabriel@gmail"));
+            $this->assertEquals(true, isValidEmail("gabriel.trevisan.santos@gmail.com"));
+        }
+
     }
 
 ?>
